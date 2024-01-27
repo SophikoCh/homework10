0fda1de023af400ede9f8871c4ac3cc98d86a8ad
Sopho Chitadze <chitadzesopho2@gmail.com>
2024-01-27 09:19:48 +0400

    switch and while

---
+++ main.js
@@ -0,0 +1,56 @@
+// 1 davaleba
+// let browser = prompt("What is your browser?")
+// if (browser === "edge!") {
+//     alert("You've got the Edge")
+// } else if (browser === "chrome" || browser === "firefox" || browser === "safari" || browser === "opera") {
+//     alert('Okay we support these browsers too')
+// } else {
+//     alert("We hope that ths page look ok!")
+// }
+
+//2 davaleba
+// let number1 = prompt("Enter first number")
+// let number2 = prompt("Enter second number")
+// let operator = prompt("Enter operator")
+
+// let sum = +number1 + +number2
+// let sub = +number1 - +number2
+// let mul = +number1 * +number2
+// let div = +number1 / +number2
+
+// switch (operator) {   
+//     case "+":
+//         alert(sum);
+//         break;
+//     case "-":
+//         alert(sub);
+//         break;
+//     case "*":
+//         alert(mul);
+//         break;
+//     case "/":
+//         alert(div);
+//         break;
+//     default: 
+//     alert("Invalid operator")
+// }
+
+//3 davaleba 
+
+
+
+
+//4 davaleba
+// let userInput = prompt("Enter a number:");
+// let number = parseInt(userInput);
+// let sumOfDigits = 0;
+
+// while (number !== 0) {
+//     let digit = number % 10;
+//     sumOfDigits += digit;
+//     number = (number - digit) / 10;
+// } alert("Sum of digits: " + sumOfDigits);
+
+
+
+
